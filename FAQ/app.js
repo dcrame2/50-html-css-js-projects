6d1a5118ce93ef8cb988cc9ed1accb6bea412651
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-07-03 17:15:33 -0500

    update

--- FAQ/app.js
+++ FAQ/app.js
@@ -1,11 +1,7 @@
-const toggles = document.querySelectorAll('.faq-toggle');
+const toggles = document.querySelectorAll(".faq-toggle");
 
-toggles.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active');
-    })
-})
-
-
-
-
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
